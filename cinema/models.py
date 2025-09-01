fb33ba8a2fd4bce99925f19796a3c4125d8f5f34
Hetmarum <kpxmedet@gmail.com>
2025-09-01 08:22:30 +0300

    another ai fix

--- cinema/models.py
+++ cinema/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=63, unique=True)
+    title = models.CharField(max_length=63)
     description = models.TextField(blank=True, null=True)
     duration = models.PositiveIntegerField()
 
